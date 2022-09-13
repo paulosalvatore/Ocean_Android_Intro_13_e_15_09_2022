0b92ee0679d0693e01262b7db574255fed291bc4
Paulo Salvatore <salvatoregames@gmail.com>
2022-09-13 11:44:36 -0300

    Funcionamento do `Button` atualizando `TextView`

--- app/src/main/java/com/oceanbrasil/ocean_android_intro_13_e_15_09_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_intro_13_e_15_09_2022/MainActivity.kt
@@ -2,10 +2,29 @@ package com.oceanbrasil.ocean_android_intro_13_e_15_09_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Detectar o clique no botão
+
+        // Pegamos o elemento Button na tela, pelo ID
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+
+        // Pegamos o elemento TextView na tela, pelo ID
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        // Adicionamos um listener para o btEnviar
+        btEnviar.setOnClickListener {
+            // Tudo que escrevermos dentro deste bloco será executado
+            // assim que o Button for clicado
+
+            // Alterar o texto do TextView
+            tvResultado.text = "Paulo Salvatore"
+        }
     }
 }
