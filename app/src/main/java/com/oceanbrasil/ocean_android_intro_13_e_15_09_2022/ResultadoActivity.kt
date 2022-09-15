84b2d92b046d6ec79253bb15df35a79d56c2aa4b
Paulo Salvatore <salvatoregames@gmail.com>
2022-09-15 09:43:05 -0300

    Criação da `ResultadoActivity`

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_intro_13_e_15_09_2022/ResultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.ocean_android_intro_13_e_15_09_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
