21c2b548bb96f88b5dfc6171f13725c5d96c8474
Paulo Salvatore <salvatoregames@gmail.com>
2022-09-15 10:25:06 -0300

    Funcionamento do `btVoltar` na `ResultadoActivity`

--- app/src/main/java/com/oceanbrasil/ocean_android_intro_13_e_15_09_2022/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_intro_13_e_15_09_2022/ResultadoActivity.kt
@@ -2,10 +2,17 @@ package com.oceanbrasil.ocean_android_intro_13_e_15_09_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+
+        btVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
